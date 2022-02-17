d36babf6f13cead0921fcbb3a3084ed9371c4b08
Georg Lang <georglang0788@gmx.net>
2022-02-17 19:31:26 +0100

    csv record class

---
+++ src/app/csv-parser/csv-record.ts
@@ -0,0 +1,28 @@
+export class CSVRecord {
+  // csv fields
+  public Datum: any;
+  public Von: any;
+  public Bis: any;
+  public Dauer: any;
+  public Lohn: any;
+  public mobile: any;
+  public "Interner Lohn": any;
+  public Benutzer: any;
+  public Name: any;
+  public Kunde: any;
+  public Projekt: any;
+  public Tätigkeit: any;
+  public Beschreibung: any;
+  public Exportiert: any;
+  public Abrechenbar: any;
+  public Schlagworte: any;
+  public Stundenlohn: any;
+  public Festbetrag: any;
+  public Typ: any;
+  public "label.category": any;
+  public Kundennummer: any;
+  public "Umsatzsteuer-ID": any;
+  public Bestellnummer: any;
+  // not a csv field
+  public Nachname: any
+}
